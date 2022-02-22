d1e373bc8b78ef07e66f976b14e273ab4f7de5d6
Igor Kotrasinski <i.kotrasinsk@gmail.com>
2022-02-22 21:28:35 +0100

    Input/output args
    Signed-off-by: Igor Kotrasinski <i.kotrasinsk@gmail.com>

--- src/args.rs
+++ src/args.rs
@@ -5,6 +5,12 @@ use clap::{Parser, Subcommand};
 pub struct Args {
     #[clap(subcommand)]
     pub command: Commands,
+    /// Input file. If absent or '-', reads standard input.
+    #[clap(short, long)]
+    pub input: Option<String>,
+    /// Output file. If absent or '-', writes to standard output.
+    #[clap(short, long)]
+    pub output: Option<String>,
 }
 
 #[derive(Subcommand, Debug)]
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io::Read;
+use std::io::{Read, Write};
 use args::Commands;
 use blueprint::Blueprint;
 use clap::StructOpt;
@@ -10,33 +10,50 @@ mod md5;
 mod bpdata;
 mod serialize;
 
+fn iof(arg: &Option<String>) -> Option<&str> {
+    match arg.as_ref().map(|x| x.as_ref()) {
+        None | Some("-") => None,
+        file => file,
+    }
+}
+
 fn main() -> anyhow::Result<()> {
     let args = args::Args::parse();
 
+    let mut input: Box<dyn Read> = match iof(&args.input) {
+        None => Box::new(std::io::stdin()),
+        Some(file) => Box::new(std::fs::File::open(file)?),
+    };
+    let mut output: Box<dyn Write> = match iof(&args.output) {
+        None => Box::new(std::io::stdout()),
+        Some(file) => Box::new(std::fs::OpenOptions::new()
+                          .write(true)
+                          .truncate(true)
+                          .create(true)
+                          .open(file)?),
+    };
+
     match args.command {
         Commands::Parse => {
-            let mut stdin = std::io::stdin();
             let mut data = vec!();
-            stdin.read_to_end(&mut data)?;
+            input.read_to_end(&mut data)?;
             let data = String::from_utf8(data)?;
             let bp = Blueprint::new(&data)?;
-            print!("{}", bp.into_bp_string()?);
+            output.write_all(bp.into_bp_string()?.as_bytes())?;
         }
         Commands::Dump => {
-            let mut stdin = std::io::stdin();
             let mut data = vec!();
-            stdin.read_to_end(&mut data)?;
+            input.read_to_end(&mut data)?;
             let data = String::from_utf8(data)?;
             let bp = Blueprint::new(&data)?;
-            print!("{}", std::str::from_utf8(&bp.dump_json()?)?);
+            output.write_all(&bp.dump_json()?)?;
         }
         Commands::Undump => {
-            let mut stdin = std::io::stdin();
             let mut data = vec!();
-            stdin.read_to_end(&mut data)?;
+            input.read_to_end(&mut data)?;
             let data = String::from_utf8(data)?;
             let bp = Blueprint::new_from_json(&data)?;
-            print!("{}", bp.into_bp_string()?);
+            output.write_all(bp.into_bp_string()?.as_bytes())?;
         }
     }
     Ok(())
