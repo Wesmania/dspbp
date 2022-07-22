bc3c20013b9d39ac46b6b4ee2adc9b33258e5e1b
Igor Kotrasinski <i.kotrasinsk@gmail.com>
2022-07-22 22:55:40 +0200

    Experiment with gh actions
    Signed-off-by: Igor Kotrasinski <i.kotrasinsk@gmail.com>

--- setup.py
+++ setup.py
@@ -2,9 +2,8 @@ from setuptools import setup
 from setuptools_rust import Binding, RustExtension
 
 setup(
-    name="dspbp",
-    version="0.0.3",
+    author="Wesmania",
+    author_email="ikotrasinsk@gmail.com",
     rust_extensions=[RustExtension("dspbp", binding=Binding.PyO3, features=["python"])],
     zip_safe=False,
-    python_requires=">=3.7",
 )
